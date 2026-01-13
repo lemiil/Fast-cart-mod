d508cca6708b1c138b506953386f846ade38e713
lemiil <gavriillemeshko10@gmail.com>
2026-01-13 19:30:33 +0200

    Change

--- src/main/java/net/lemiil/fastcart/FastCart.java
+++ src/main/java/net/lemiil/fastcart/FastCart.java
@@ -5,6 +5,7 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+//Change
 public class FastCart implements ModInitializer {
 	public static final String MOD_ID = "fast-cart";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
