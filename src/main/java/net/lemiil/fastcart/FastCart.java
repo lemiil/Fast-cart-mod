4df98bf3f3612f5bc2f4eba5c80d76f1afb46746
lemiil <gavriillemeshko10@gmail.com>
2026-01-13 19:29:17 +0200

    Init

---
+++ src/main/java/net/lemiil/fastcart/FastCart.java
@@ -0,0 +1,16 @@
+package net.lemiil.fastcart;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FastCart implements ModInitializer {
+	public static final String MOD_ID = "fast-cart";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/lemiil/fastcart/FastCartClient.java
@@ -0,0 +1,11 @@
+package net.lemiil.fastcart;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FastCartClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
